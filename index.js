6737664685edc91dbd5e2b238a20d228e1bf3f7e
yann <yann.boisselier@gmail.com>
2019-01-04 00:04:09 +0100

    use only sync operations for now

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const { runImporters } = require('./lib/helpers')
 
 module.exports = function Gustave() {
   this.nuxt.hook('build:before', () => {
-    const routes = runGustave()
+    const routes = runImporters()
     this.options.generate.routes = [...routes, ...this.options.generate.routes]
   })
-}
-
-function runGustave() {
-  console.log('ℹ Launching gustave importers ')
-  const routes = runImporters()
-  return routes
 }
 
 module.exports.meta = require('./package.json')
